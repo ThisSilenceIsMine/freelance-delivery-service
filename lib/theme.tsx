ff5e744908dfd7e51f2998a7b85e01193901978d
ThisSilenceIsMine <49694311+ThisSilenceIsMine@users.noreply.github.com>
2021-04-19 19:33:20 +0300

    Messing with emotion and storybook

---
+++ lib/theme.tsx
@@ -0,0 +1,8 @@
+export const theme = {
+  LIGHT: {
+    color: {
+      orange: 'hsl(39, 100%, 50%)',
+      grey: 'hsl(0, 0%, 86%)',
+    }
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import { AppProps } from "next/dist/next-server/lib/router/router";
-import "../styles/globals.css";
+import { css, Global, Theme, ThemeProvider } from '@emotion/react';
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+import { theme } from '../lib/theme'; //usage may change, if you want to implement theming
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme.LIGHT}>
+      <Global
+        styles={css`
+          @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+        `}
+      />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
